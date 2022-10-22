cac3e99b21e6b1eb834671b01ce6671cca30997e
Arthur Henrique <77027468+arthurhenrique22@users.noreply.github.com>
2022-10-21 22:40:26 -0300

    Add files via upload

---
+++ ex_3/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Arthur Henrique Alves Saturno</h1>
+        <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
